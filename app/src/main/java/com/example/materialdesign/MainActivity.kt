36d4af4648837110f68b473d4ea2f74508aa31f7
Fernando da Costa Felício <ffelicio@autodoc.com.br>
2018-09-05 14:47:00 -0300

    Estudo dos recursos do material design - Button

--- app/src/main/java/com/example/materialdesign/MainActivity.kt
+++ app/src/main/java/com/example/materialdesign/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.materialdesign
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        callIntentFloatingActionButton()
+    }
+
+    // Chamando a activity com a referência Floating Action Button
+    fun callIntentFloatingActionButton() {
+        val buttonIntentButtonFab = button_call_intent_button_fab
+
+        buttonIntentButtonFab.setOnClickListener {
+            val intent = Intent(this@MainActivity, FloatingActionButtonActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
